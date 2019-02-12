7e7bcc2f801849db68a50619c7d37d75d047289c
Asbjørn Madsen <asbjoern1982@gmail.com>
2019-02-12 14:30:11 +0100

    diveded by zero fixed

--- src/stages/game/config/score.js
+++ src/stages/game/config/score.js
@@ -6,13 +6,14 @@ export default {
     // let score = 10 + colony.inventory.filter(row => row.amount > 500).map(row => 2).reduce((total, num) => total + num)
     let score = colony.dead ? 0 : colony.inventory
       .map(row => row.amount)
-      .reduce((total, num) => total + num)
+      .reduce((total, num) => total + num, 0)
     let totalMaterials = colonies.filter(col => !col.dead)
       .map(col => col.inventory)
       .map(inventory => inventory
         .map(row => row.amount)
-        .reduce((total, num) => total + num))
-      .reduce((total, num) => total + num)
+        .reduce((total, num) => total + num, 0))
+      .reduce((total, num) => total + num, 0)
+    if (totalMaterials === 0) totalMaterials = 1 // could be zero
     let procent = Math.floor(score / totalMaterials * 100)
     let payout = Math.floor((upperlimit - lowerlimit) * (score / totalMaterials) + lowerlimit)
     return payout + ' kr (' + procent + '%)'
