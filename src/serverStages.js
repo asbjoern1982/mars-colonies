d7e4fc88c1c0440f549de80d41a9d91c4d2af0b4
ninja <asbjoern1982@gmail.com>
2019-07-15 16:46:04 +0200

    start with a game

--- src/serverStages.js
+++ src/serverStages.js
@@ -4,15 +4,16 @@ import payment from './stages/paymentmodule/server/server'
 
 import preSurvey from './stages/survey/config/presurvey.json'
 import postSurvey from './stages/survey/config/postsurvey.json'
-import round1 from './stages/game/config/config.json'
+import round1config from './stages/game/config/config.json'
+import round1score from './stages/game/config/score'
 import round2 from './stages/game/config/config.json'
 import round3 from './stages/game/config/config.json'
 
 let configurations = [
-  {stage: survey, config: preSurvey},
-  {stage: game, config: round1},
+  //{stage: survey, config: preSurvey},
+  {stage: game, config: {config: round1config, score: round1score}},
   {stage: survey, config: postSurvey, },
-  {stage: payment}
+  {stage: payment, config: undefined}
 ]
 
 export default {
