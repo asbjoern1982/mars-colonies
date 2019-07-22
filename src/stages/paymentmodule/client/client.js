49e03737daa819d054a8a28bb2ba0db238db0b8f
ninja <asbjoern1982@gmail.com>
2019-07-22 10:08:06 +0200

    height fixed

--- src/stages/paymentmodule/client/client.js
+++ src/stages/paymentmodule/client/client.js
@@ -20,6 +20,12 @@ export default {
     }
   },
   setup: (client) => {
+    let link = document.querySelector("link[rel*='icon']") || document.createElement('link')
+    link.type = 'image/x-icon'
+    link.rel = 'shortcut icon'
+    link.href = './../../../assets/favicon.ico'
+    document.getElementsByTagName('head')[0].appendChild(link)
+    
     client.send('ready')
   },
   teardown: (client) => {},
