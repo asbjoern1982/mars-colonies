7c79db519000e2a399d1733ff2491fb5dfa3ce6f
Asbjørn Madsen <asbjoern1982@gmail.com>
2020-02-19 14:14:27 +0100

    tuned the stategy, added a gameloop and added comments

--- src/bot.js
+++ src/bot.js
@@ -10,74 +10,99 @@ socket.on('disconnect', () => {
 socket.on('error', err => console.log(err))
 socket.on('event', handleEvent)
 
-let startingInformation
-
+let materials
 let myName
-let productionIngredient
-let productionOutput
-let productionOutputStartingAmount
+let colonies
+let input
+let output
+let inputStartAmount
+let outputStartAmount
 
-let recent = true
+let producing = false
+let gameloop = () => {
+  // update inventories
+  colonies.forEach(colony => {
+    colony.inventory.forEach(line => {
+      colonies.find(col => col.name === colony.name).inventory.find(lin => lin.name === line.name).amount -= materials.find(mat => mat.name === line.name).depletion_rate
+    })
+  })
+
+  // as these are fairly long calls we will have local variables here:
+  let inputCurrentAmount = colonies.find(colony => colony.name === myName).inventory.find(line => line.name === input).amount
+  let outputCurrentAmount = colonies.find(colony => colony.name === myName).inventory.find(line => line.name === output).amount
+
+  // production
+  // if we have more than 1400 input, produce down to 1200, else produce down to 500
+  let inputTarget = inputCurrentAmount > 1.4 * inputStartAmount ? 1.2 * inputStartAmount : 0.5 * inputStartAmount
+  if (!producing && inputCurrentAmount > inputTarget) {
+    // first lock production so we only tell the server to produce when possible
+    producing = true
+    setTimeout(() => producing = false, 11000) // NOTE: the bot asumes a 10 production second delay and no transfer-delay
+
+    // produce everything down to inputTarget
+    let amount = Math.round(inputCurrentAmount - inputTarget)
+    console.log('starting producing ' + amount)
+    socket.emit('event', { type: 'produce', payload: {
+      index: 0,
+      amount: amount}
+    })
+  }
+
+  // transfer
+  // only transfer what is more than what we started with of our output-material times 1.2
+  let available = outputCurrentAmount - 1.2 * outputStartAmount
+  if (available > 0) {
+    // first find colonies in need
+    let coloniesInNeedList = colonies
+      .filter(colony => colony.name !== myName)
+      .filter(colony => colony.inventory.find(line => line.name === output).amount < outputStartAmount)
+
+    // find their total amount of output-material
+    let totalMissing = coloniesInNeedList
+      .map(colony => outputStartAmount - colony.inventory.find(line => line.name === output).amount)
+      .reduce((total, num) => total + num, 0)
+
+    coloniesInNeedList.forEach(colony => {
+      // send an amount to them proportional to what they are missing to have what they started with
+      let colonyOutputCurrentAmountMissing = outputStartAmount - colony.inventory.find(line => line.name === output).amount
+      let amount = Math.round(available * colonyOutputCurrentAmountMissing / totalMissing)
+
+      socket.emit('event', { type: 'trade', payload: {
+        colony: colony.name,
+        material: output,
+        amount: amount
+      }})
+    })
+  }
+}
 
 function handleEvent (event) {
   if (event.type === '@monsterr/START_STAGE') {
+    // tell the server that we are a robot and are ready as we are succesfully connected
     socket.emit('event', { type: 'ready', payload: 'bot' })
     console.log('detected stage start, reported ready to the server')
   } else if (event.type === 'setup') {
+    // getting a setup message means that the game has started and it contains all the information that we need
     console.log('setup data received')
-    startingInformation = event.payload
 
+    materials = event.payload.materials
     myName = event.payload.yourName
-    productionOutput = event.payload.yourSpecializations[0].output
-    productionOutputStartingAmount = event.payload.yourStartingInventory.find(inv => inv.name === productionOutput).amount
+    colonies = event.payload.colonies
+    input = event.payload.yourSpecializations[0].input
+    output = event.payload.yourSpecializations[0].output
+    inputStartAmount = event.payload.yourStartingInventory.find(inv => inv.name === input).amount
+    outputStartAmount = event.payload.yourStartingInventory.find(inv => inv.name === output).amount
 
-
-    // console.log(event.payload);
-
-    // produce half of startingIventory
-    socket.emit('event', { type: 'produce', payload: {
-      index: 0,
-      amount: Math.round(productionOutputStartingAmount / 2)}
-    })
-    setTimeout(() => recent = false, 12000)
+    // start the internal gameloop
+    setInterval(gameloop, 1000)
   } else if (event.type === 'inventories') {
-    console.log('recieved inventory update, my inventory: ' + event.payload.find(colony => colony.name === myName).inventory.map(line => Math.round(line.amount)).join())
+    // update our inventories
+    console.log('recieved inventory update')
 
-    if (recent)
-      return
-    recent = true
-
-    let otherColoniesTotalInventory = event.payload
-      .filter(colony => colony.name !== myName)
-      .map(colony => colony.inventory.find(line => line.name === productionOutput).amount)
-      .reduce((total, num) => total + num, 0)
-
-    let avalibleAmount =  event.payload
-      .filter(colony => colony.name === myName)
-      .map(colony => colony.inventory.find(line => line.name === productionOutput).amount) - productionOutputStartingAmount
-
-    event.payload.filter(colony => colony.name !== myName).forEach(colony => {
-      let targetInventory = colony.inventory.find(line => line.name === productionOutput).amount
-      let transferAmount =  avalibleAmount * (productionOutputStartingAmount - targetInventory) / otherColoniesTotalInventory
-      if (transferAmount > 0) { // negative if target has more than starting
-        console.log('transfering ' + transferAmount + ' to ' + colony.name + ' that has ' + targetInventory)
-        socket.emit('event', { type: 'trade', payload: {
-          colony: colony.name,
-          material: productionOutput,
-          amount: Math.round(transferAmount)
-        }})
-      }
+    colonies.forEach(colony => {
+      colony.inventory = event.payload.find(line => line.name === colony.name).inventory
     })
-
-    // produce half of inventory
-    let ingreidentInventory = event.payload.find(colony => colony.name === myName).inventory.find(line => line.name === productionOutput).amount
-    socket.emit('event', { type: 'produce', payload: {
-      index: 0,
-      amount: Math.round(ingreidentInventory / 2)}
-    })
-
-    setTimeout(() => recent = false, 12000)
   } else {
-    console.log('event not handled: ' + event.type)
+    console.log('event not handled: "' + event.type + '" with [' + event.payload + ']')
   }
 }
